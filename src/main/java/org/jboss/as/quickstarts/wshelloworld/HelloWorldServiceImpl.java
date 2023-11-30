64e6bbb46764996adebae313e160f2dbec025a22
aabualru <aabualru@redhat.com>
2023-11-30 19:00:31 +0000

    Updated HelloWorldServiceImpl.java

--- src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -32,7 +32,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String sayHello() {
-        return "Hello World!";
+        return "Hello Red Hat!";
     }
 
     @Override
